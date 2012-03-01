49696c62f426076081bf0185479986d5c429b0a6
Walt Grata <wegrata@gmail.com>
2012-03-01 13:53:07 -0500

    Added forward paging

--- src/landing.py
+++ src/landing.py
@@ -17,33 +17,50 @@ class DocHandler(Jinja2Rendering):
         return self.render_template('doc.html',doc=doc)
 
 class DocsHandler(Jinja2Rendering):
-    def get(self):
-        client = redis.Redis()    
-        ids = client.lrange('ids',0,-1)        
-        return self.render_template('docs.html',ids=ids)
+    def get(self):    
+        try:
+            start = int(self.get_argument('pos'))
+        except:
+            start = 0      
+        ids = self.db_conn.lrange('ids',start,start+100)        
+        args = {'ids':ids}
+        if len(ids) > 100:
+          args['nextUrl'] ="/docs?pos="+str(start+100)
+        return self.render_template('docs.html',**args)
 
 
 class IndexHandler(Jinja2Rendering):
-  def get(self):
+    def get(self):
         return self.render_template('index.html')
 
 
 class KeysHandler(Jinja2Rendering):
   def get(self):
-      client = redis.Redis()    
-      keys = client.lrange('keys',0,-1)
-      return self.render_template('keys.html',keys=keys)
+      try:
+          start = int(self.get_argument('pos'))
+      except:
+          start = 0
+      keys = self.db_conn.lrange('keys',start,start+100)
+      args = {'keys':keys}
+      if len(keys) > 100:
+        args['pos']=str(start+100)
+      return self.render_template('keys.html',**args)
 
 class KeyHandler(Jinja2Rendering):
     def get(self,keyword):
-        client = redis.Redis()    
         keyword = urllib.unquote(keyword)
-        ids = client.lrange(keyword,0,-1)    
-        return self.render_template('docs.html',ids=ids)
+        try:
+            start = int(self.get_argument('pos'))
+        except:
+            start = 0      
+        ids = self.db_conn.lrange(keyword,start,start+100)        
+        args = {'ids':ids}
+        if len(ids) > 100:
+          args['nextUrl'] = nextUrl="/keys/%s?pos=%d" % (keyword,start+100)
+        return self.render_template('docs.html',**args)
 class SitemapHandler(Jinja2Rendering):
     def get(self):
-        client = redis.Redis()    
-        ids = client.lrange('ids',0,-1) 
+        ids = self.db_conn.lrange('ids',0,-1) 
         self.headers["Conent-Type"] = 'text/xml'
         return self.render_template('sitemap.xml',ids=ids)      
 urls = [(r'^/sitemap.xml',SitemapHandler),
@@ -57,5 +74,6 @@ mongrel2_pair = ('tcp://127.0.0.1:9997', 'tcp://127.0.0.1:9996')
 
 app = Brubeck(mongrel2_pair=mongrel2_pair,
               handler_tuples=urls,
+              db_conn = redis.Redis(),
               template_loader=load_jinja2_env('./templates'))
 app.run()
